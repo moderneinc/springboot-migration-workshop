93109f9d94773757060ac29592c54d7f4a353aa4
Tim te Beek <tim@moderne.io>
2023-11-01 19:11:53 +0200

    Bump versions.sh

--- src/scripts/versions.sh
+++ src/scripts/versions.sh
@@ -11,18 +11,18 @@ function replace() {
     grep --recursive --files-with-matches "$1" docs | xargs --no-run-if-empty sed --in-place "s/$1/$2/g"
   fi
 }
-replace 'VERSION_SDKMAN_JAVA8'            '8.0.382-zulu'
-replace 'VERSION_SDKMAN_JAVA11'           '11.0.20-tem'
-replace 'VERSION_SDKMAN_JAVA17'           '17.0.8.1-tem'
-replace 'VERSION_SPRING_BOOT27'           '2.7.16'
-replace 'VERSION_SPRING_BOOT31'           '3.1.4'
-replace 'VERSION_REWRITE_GRADLE_PLUGIN'   '6.3.18'
-replace 'VERSION_REWRITE_MAVEN_PLUGIN'    '5.7.2'
-replace 'VERSION_REWRITE_SPRING'          '5.0.11'
+replace 'VERSION_SDKMAN_JAVA8'            '8.0.392-zulu'
+replace 'VERSION_SDKMAN_JAVA11'           '11.0.21-tem'
+replace 'VERSION_SDKMAN_JAVA17'           '17.0.9-tem'
+replace 'VERSION_SPRING_BOOT27'           '2.7.17'
+replace 'VERSION_SPRING_BOOT31'           '3.1.5'
+replace 'VERSION_REWRITE_GRADLE_PLUGIN'   '6.4.3'
+replace 'VERSION_REWRITE_MAVEN_PLUGIN'    '5.9.2'
+replace 'VERSION_REWRITE_SPRING'          '5.0.12'
 replace 'VERSION_RECIPE_SPRING_BOOT'      'UpgradeSpringBoot_3_1'
 replace 'VERSION_MIGRATE_SPRING_BOOT'     'Migrate to Spring Boot 3.1'
 replace 'VERSION_LINK_SPRING_BOOT'        'upgradespringboot_3_1'
-replace 'VERSION_MODERNE_CLI'             'v1.1.0'
+replace 'VERSION_MODERNE_CLI'             'v1.4.10'
 
 if [ -n "$inverse" ]; then
   echo "Replaced all versions with placeholders"
