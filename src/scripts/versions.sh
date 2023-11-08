0384d4e5e3bd913557cf2f1b966110e3d314b83d
Tim te Beek <tim@moderne.io>
2023-11-08 09:29:10 +0100

    Bump versions one last time before export

--- src/scripts/versions.sh
+++ src/scripts/versions.sh
@@ -16,13 +16,13 @@ replace 'VERSION_SDKMAN_JAVA11'           '11.0.21-tem'
 replace 'VERSION_SDKMAN_JAVA17'           '17.0.9-tem'
 replace 'VERSION_SPRING_BOOT27'           '2.7.17'
 replace 'VERSION_SPRING_BOOT31'           '3.1.5'
-replace 'VERSION_REWRITE_GRADLE_PLUGIN'   '6.4.3'
-replace 'VERSION_REWRITE_MAVEN_PLUGIN'    '5.9.2'
+replace 'VERSION_REWRITE_GRADLE_PLUGIN'   '6.4.4'
+replace 'VERSION_REWRITE_MAVEN_PLUGIN'    '5.9.3'
 replace 'VERSION_REWRITE_SPRING'          '5.0.12'
 replace 'VERSION_RECIPE_SPRING_BOOT'      'UpgradeSpringBoot_3_1'
 replace 'VERSION_MIGRATE_SPRING_BOOT'     'Migrate to Spring Boot 3.1'
 replace 'VERSION_LINK_SPRING_BOOT'        'upgradespringboot_3_1'
-replace 'VERSION_MODERNE_CLI'             'v1.4.11'
+replace 'VERSION_MODERNE_CLI'             'v1.6.1'
 
 if [ -n "$inverse" ]; then
   echo "Replaced all versions with placeholders"
